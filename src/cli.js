0bd87d41ffeed5aa9171dc96d4bea5955246f20f
sukritcharredsoul <sukritchoudhary16@gmail.com>
2026-03-27 15:41:46 +0530

    Added CLI working via Commander

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,41 @@
-// #!/usr/bin/env node
-
-// import {command} from commander ;
-// import { certChecker } from "./services/certChecker";
-
-// const program = new command() ;
-
-// // program
-// //     .name('tls-certificate')
-// //     .description('Check TLS/SSL Certificate for Domain')
-// //     .version('1.0.0') ;
+#!/usr/bin/env node
 
 
-// // program
+import {Command} from 'commander' ;
+import { certChecker } from "./services/certChecker.js";
+
+const program = new Command() ;
+
+program
+    .name('tls-certificate')
+    .description('Check TLS/SSL Certificate for Domain')
+    .version('1.0.0') ;
+
+
+program
+    .argument('<hostname>',"Domain to check")
+    .option('-p,--port <port>',"Port Number",'443')
+    .action(async (hostname,options) => {
+        try {
+            const result = await certChecker(hostname,parseInt(options.port)) ;
+            console.log("Domain : "+ hostname);
+            console.log("Subject :" + JSON.stringify(result.cert.subject)) ;
+            console.log("validTill : " + result.cert.validTill) ;
+            console.log("validfrom : " + result.cert.validfrom ) ;
+            console.log("fingerPrint : " + result.cert.fingerprint ) ;
+            console.log('TLS Version : ' + result.protocol)
+            console.log('Cipher      : ' + result.cipher.name)
+
+
+        if (result.cert.isExpired) {
+            console.log("Certificate is EXPIRED");
+        } else {
+            console.log("Certificate is valid");
+        }} catch (error) {
+            console.log(error.message) ;
+            console.log("Error Occurred while working.")
+        }
+    }) ;
+
+    program.parse() ;
 
